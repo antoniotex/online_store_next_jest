7e3aa9b2371f7f5e9f75893414ea16fe7438b44b
Antonio Carlos <antoniotx.dev@gmail.com>
2023-01-19 17:00:05 -0300

    Garante que produtos e categ. estao renderizando

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import Home from "../pages/index";
-import { render, screen } from "@testing-library/react";
+import { getAllByRole, render, screen, within } from "@testing-library/react";
+import { mockProducts } from "../__mocks__/mockProducts";
+import { mockCategories } from "../__mocks__/mockData";
 
 describe("Home page", () => {
   it("render a all products heading", () => {
-    render(<Home />);
+    render(<Home products={mockProducts} categories={[]} />);
     screen.getByRole("heading", { name: /All products/i });
   });
 
   it("render a acessible search box", () => {
-    render(<Home />);
+    render(<Home products={mockProducts} categories={[]} />);
     const searchInput = screen.getByRole("searchbox");
     expect(searchInput).toHaveAccessibleName("Search");
   });
+
+  it("renders all products", () => {
+    render(<Home products={mockProducts} categories={[]} />);
+    const allProducts = screen.getByRole("list", { name: /products/i });
+    const { getAllByRole } = within(allProducts);
+    const listItems = getAllByRole("listitem");
+    expect(listItems.length).toBe(mockProducts.length);
+  });
+
+  it("renders all categories", () => {
+    render(<Home products={mockProducts} categories={mockCategories} />);
+    const allCategories = screen.getByRole("list", { name: /categories/i });
+    const { getAllByRole } = within(allCategories);
+    const listItems = getAllByRole("listitem");
+    expect(listItems.length).toBe(mockCategories.length);
+  });
+  // it("renders all categories", () => {});
+  // it("renders 1 product per category", () => {});
+  // it("show search results when search item is not empty", () => {});
 });
